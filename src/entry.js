2c08f5f08275c1411e920860f6a76ab1c43a2893
AlpineIce <cholgate322@gmail.com>
2025-03-25 22:03:58 -0400

    Game container; start event

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,21 @@
 
+//should be a button event
+function startGame() {
+    //remove start button
+    let gameArea = document.getElementById("start-button-container").remove();
+
+    //TODO implement setup logic in here
+    let gameContainer = document.getElementById("game-container");
+
+    let exampleElement = document.createElement("p");
+    exampleElement.innerText = ("Example game start functionality... TODO");
+    gameContainer.appendChild(exampleElement);
+}
 
 //entry point function
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Hello world");
+document.addEventListener("DOMContentLoaded", () => {
+    //start button event; entry point of a game
+    document.getElementById("start-button").addEventListener("click", () => {
+        startGame();
+    });
 });
