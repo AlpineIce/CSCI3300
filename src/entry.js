2d9a54a75c730ad26e71ac85627e18a6b5e2b13d
alpineice <cholgate322@gmail.com>
2025-04-02 08:50:50 -0400

    uncomment remove start button

--- src/entry.js
+++ src/entry.js
@@ -3,7 +3,7 @@ import { initializeGui } from "./gui.js";
 //should be a button event
 function startGame() {
     //remove start button
-    //document.getElementById("start-button-container").remove();
+    document.getElementById("start-button-container").remove();
 
     //retrieve game container
     let gameContainer = document.getElementById("game-container");
